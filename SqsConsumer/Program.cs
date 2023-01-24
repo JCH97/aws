b3df041133ebcbfa2ad01131bfd1a0641b688a39
JCH97 <jc997hdez@gmail.com>
2023-01-23 22:32:58 -0500

    feat(): add aws publisher. add aws consumer.

---
+++ SqsConsumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var cts = new CancellationTokenSource();
+
+var credentials = new BasicAWSCredentials("AKIAXPGQ73HA3F62LPH6", "hk+keLq5JWUr6Ea/9iuxLmnynSrVcxnQM8bR5rKZ");
+var awsClient = new AmazonSQSClient(credentials, RegionEndpoint.USEast1);
+
+var queueUrl = await awsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrl.QueueUrl,
+    AttributeNames = new List<string> { "All" },
+    MessageAttributeNames = new List<string>
+        { "All" } // retrieve all attributes, it could be util in order to facilitate events in consumers
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await awsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        // await awsClient.DeleteMessageAsync(queueUrl.QueueUrl, message.ReceiptHandle);
+    }
+
+    // await Task.Delay(3000);
+}
+
+public record CustomerCreated(string name, string lastName, int age);
---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Text.Json;
+using Amazon;
+using Amazon.Runtime;
+
+
+var credentials = new BasicAWSCredentials("AKIAXPGQ73HA3F62LPH6", "hk+keLq5JWUr6Ea/9iuxLmnynSrVcxnQM8bR5rKZ");
+
+var awsClient = new AmazonSQSClient(credentials, RegionEndpoint.USEast1);
+
+var queueUrl = await awsClient.GetQueueUrlAsync("customers");
+
+var customer = new CustomerCreated("Jose", "Hdez", 21);
+
+var sendMessage = new SendMessageRequest
+{
+    QueueUrl = queueUrl.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+
+    // attributes => this field be able to indicate the event-type, it could be util to filter the messages in consumers 
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue() { DataType = "String", StringValue = "CustomerCreated" }
+        }, // key - value
+    },
+};
+
+await awsClient.SendMessageAsync(sendMessage);
+
+Console.WriteLine("Message sent");
+
+public record CustomerCreated(string name, string lastName, int age);
